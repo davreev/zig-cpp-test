b8ad13dbf933aac876fc23d6f6134811b1582aef
David Reeves <reevdav@gmail.com>
2024-02-25 22:32:35 +0000

    Move source files

--- build.zig
+++ build.zig
@@ -7,49 +7,49 @@ pub fn build(b: *std.Build) void {
     //////////////////////////////////////////////
     // C++ executable calling into Zig library
 
-    const lib_zig = b.addStaticLibrary(.{
+    const hello_lib_zig = b.addStaticLibrary(.{
         .name = "hello-lib-zig",
-        .root_source_file = .{ .path = "hello_lib.zig" },
+        .root_source_file = .{ .path = "src/hello_lib.zig" },
         .target = target,
         .optimize = optimize,
     });
 
-    const exe_cpp = b.addExecutable(.{
+    const hello_cpp = b.addExecutable(.{
         .name = "hello-cpp",
         .target = target,
         .optimize = optimize,
     });
-    exe_cpp.addCSourceFile(.{
-        .file = .{ .path = "hello.cpp" },
+    hello_cpp.addCSourceFile(.{
+        .file = .{ .path = "src/hello.cpp" },
         .flags = &.{"-std=c++17"},
     });
-    exe_cpp.linkLibCpp();
-    exe_cpp.linkLibrary(lib_zig);
+    hello_cpp.linkLibCpp();
+    hello_cpp.linkLibrary(hello_lib_zig);
 
-    b.installArtifact(exe_cpp);
+    b.installArtifact(hello_cpp);
 
     //////////////////////////////////////////////
     // Zig executable calling into C++ library
 
-    const lib_cpp = b.addStaticLibrary(.{
+    const hello_lib_cpp = b.addStaticLibrary(.{
         .name = "hello-lib-cpp",
         .target = target,
         .optimize = optimize,
     });
-    lib_cpp.addCSourceFile(.{
-        .file = .{ .path = "hello_lib.cpp" },
+    hello_lib_cpp.addCSourceFile(.{
+        .file = .{ .path = "src/hello_lib.cpp" },
         .flags = &.{"-std=c++17"},
     });
-    lib_cpp.linkLibCpp();
+    hello_lib_cpp.linkLibCpp();
 
-    const exe_zig = b.addExecutable(.{
+    const hello_zig = b.addExecutable(.{
         .name = "hello-zig",
-        .root_source_file = .{ .path = "hello.zig" },
+        .root_source_file = .{ .path = "src/hello.zig" },
         .target = target,
         .optimize = optimize,
     });
-    exe_zig.linkLibrary(lib_cpp);
-    exe_zig.addIncludePath(.{ .path = "./" });
+    hello_zig.linkLibrary(hello_lib_cpp);
+    hello_zig.addIncludePath(.{ .path = "./src/" });
 
-    b.installArtifact(exe_zig);
+    b.installArtifact(hello_zig);
 }
